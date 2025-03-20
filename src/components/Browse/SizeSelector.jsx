938868b3997e09c0f7df0654131de6a1fe7413a9
Ocamp09 <owen19campbell@gmail.com>
2025-03-20 17:57:47 -0400

    add multiple sizes to browse

---
+++ src/components/Browse/SizeSelector.jsx
@@ -0,0 +1,37 @@
+import "./SizeSelector.css";
+
+const SizeSelector = ({ size, setSize, setPrevSize }) => {
+  return (
+    <div className="size-menu">
+      <button
+        className={`${size === "small" ? "active" : ""}`}
+        onClick={() => {
+          setPrevSize(size);
+          setSize("small");
+        }}
+      >
+        Small
+      </button>
+      <button
+        className={`${size === "medium" ? "active" : ""}`}
+        onClick={() => {
+          setPrevSize(size);
+          setSize("medium");
+        }}
+      >
+        Medium
+      </button>
+      <button
+        className={`${size === "large" ? "active" : ""}`}
+        onClick={() => {
+          setPrevSize(size);
+          setSize("large");
+        }}
+      >
+        Large
+      </button>
+    </div>
+  );
+};
+
+export default SizeSelector;
