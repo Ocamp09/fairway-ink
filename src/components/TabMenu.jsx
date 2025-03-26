d7e3c862321c72c2a540fe45d5a5f0c38704237d
Ocamp09 <owen19campbell@gmail.com>
2025-03-26 12:39:17 -0400

    update tab css

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -1,43 +1,43 @@
-import "./TabMenu.css";
 import {
   BsFill1CircleFill,
   BsFill2CircleFill,
   BsFill3CircleFill,
 } from "react-icons/bs";
 import { useSession } from "../contexts/DesignContext";
+import styles from "./TabMenu.module.css";
 
 const TabMenu = () => {
   const { stage, updateStage } = useSession();
   return (
-    <div className="tab-menu">
-      <div className={stage === "design" ? "active" : ""}>
+    <div className={styles.tab_menu}>
+      <div className={stage === "design" ? styles.active : ""}>
         <h3
           onClick={() => {
             updateStage("design");
           }}
         >
-          <BsFill1CircleFill size={24} className="tab-number" />
+          <BsFill1CircleFill size={24} className="tab_number" />
           Design
         </h3>
       </div>
-      <div className={stage === "adjust" ? "active" : ""}>
+      <div className={stage === "adjust" ? styles.active : ""}>
         <h3
           onClick={() => {
             updateStage("adjust");
           }}
         >
-          <BsFill2CircleFill size={24} className="tab-number" />
+          <BsFill2CircleFill size={24} className="tab_number" />
           Adjust
         </h3>
       </div>
-      <div className={stage === "preview" ? "active" : ""}>
+      <div className={stage === "preview" ? styles.active : ""}>
         <h3
-          className="tab-text"
+          className="tab_text"
           onClick={() => {
             updateStage("preview");
           }}
         >
-          <BsFill3CircleFill size={24} className="tab-number" />
+          <BsFill3CircleFill size={24} className="tab_number" />
           Preview
         </h3>
       </div>
