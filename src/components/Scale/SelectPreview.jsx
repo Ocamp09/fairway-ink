4897be81160cc204cede6a4906bf1c0a44652592
Ocamp09 <owen19campbell@gmail.com>
2025-03-26 15:21:06 -0400

    fix select coloring

--- src/components/Scale/SelectPreview.jsx
+++ src/components/Scale/SelectPreview.jsx
@@ -23,8 +23,9 @@ const SelectPreview = () => {
     if (e.target.localName !== "path") return;
 
     const fill = selectedPath.getAttribute("fill");
+
     // if the fill color is the warning color set the opacity for resetting
-    if (fill === "#EED202") {
+    if (fill === "blue") {
       selectedPath.setAttribute("fill-opacity", 1);
     }
 
@@ -35,7 +36,7 @@ const SelectPreview = () => {
     } else {
       // if we set the fill-opacity set back to warning color, else black
       if (selectedPath.getAttribute("fill-opacity") === "1") {
-        selectedPath.setAttribute("fill", "#EED202");
+        selectedPath.setAttribute("fill", "blue");
       } else {
         selectedPath.setAttribute("fill", "black");
       }
