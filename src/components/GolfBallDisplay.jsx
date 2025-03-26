e3424115b05dee5021b54cab4031d30a3c4d9a89
Ocamp09 <owen19campbell@gmail.com>
2025-03-26 12:35:52 -0400

    update display css

--- src/components/GolfBallDisplay.jsx
+++ src/components/GolfBallDisplay.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
-import "../components/GolfBallDisplay.css";
 import ImageEditor from "./Image_Drawing/ImageEditor";
 import TabMenu from "./TabMenu";
 import ScaleSvg from "./Scale/ScaleSvg";
 import PreviewTab from "./Preview/PreviewTab";
 import { useSession } from "../contexts/DesignContext";
+import styles from "./GolfBallDisplay.module.css";
 
 const GolfBallDisplay = () => {
   const { stage } = useSession();
 
   return (
-    <div className="golf-ball-display">
+    <div className={styles.golf_ball_display}>
       <TabMenu />
       {stage === "design" && <ImageEditor />}
 
