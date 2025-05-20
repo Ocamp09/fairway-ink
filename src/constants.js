112e5f746a10a2ed4b8d1e985055d167aa78d7ca
Ocamp09 <owen19campbell@gmail.com>
2025-05-19 20:54:09 -0400

    change API port to 8000 because 5000 is used on mac

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ export const CUSTOM_PRICE = 15.99;
 export const TEXT_PRICE = 18.99;
 export const API_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:5000"
+    ? "http://localhost:8000"
     : "https://api.fairway-ink.com";
