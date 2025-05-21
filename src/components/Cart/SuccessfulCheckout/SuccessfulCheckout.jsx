99194e4c449a9c03880d13c3d15cd708951b7af2
Ocamp09 <owen19campbell@gmail.com>
2025-05-21 12:36:02 -0400

    refactor and unit test

--- src/components/Cart/SuccessfulCheckout/SuccessfulCheckout.jsx
+++ src/components/Cart/SuccessfulCheckout/SuccessfulCheckout.jsx
@@ -1,24 +1,37 @@
 import { FaCheckCircle } from "react-icons/fa";
 import styles from "./SuccessfulCheckout.module.css";
 
-const SuccessfulCheckout = ({ orderInfo }) => (
-  <div className={styles.paid}>
-    <h3>
-      <FaCheckCircle color="4BB543" size={36} /> Your order has been submitted
-    </h3>
-    <hr />
-    <label>Order info:</label>
-    <p>Email: {orderInfo.email}</p>
-    <p>Amount paid: ${orderInfo.amount / 100}</p>
-    <hr />
-    <label>Shipping info:</label>
-    <p>Carrier: {orderInfo.shipping_info.carrier}</p>
-    <p>Tracking Number: {orderInfo.shipping_info.tracking_id}</p>
-    <p>
-      Estimated Delivery {orderInfo.shipping_info.expected_delivery + 3}-
-      {orderInfo.shipping_info.expected_delivery + 5} days
-    </p>
-  </div>
-);
+const SuccessfulCheckout = ({ orderInfo }) => {
+  if (!orderInfo) return null;
+
+  const {
+    email,
+    amount,
+    shipping_info: {
+      carrier = "N/A",
+      tracking_id = "N/A",
+      expected_delivery = 0,
+    } = {},
+  } = orderInfo;
+
+  return (
+    <div className={styles.paid} data-testid="successful-checkout">
+      <h3 className={styles.successHeader}>
+        <FaCheckCircle color="4BB543" size={36} /> Your order has been submitted
+      </h3>
+      <hr />
+      <label>Order info:</label>
+      <p>Email: {email}</p>
+      <p>Amount paid: ${(amount / 100).toFixed(2)}</p>
+      <hr />
+      <label>Shipping info:</label>
+      <p>Carrier: {carrier}</p>
+      <p>Tracking Number: {tracking_id}</p>
+      <p>
+        Estimated Delivery: {expected_delivery + 3}–{expected_delivery + 5} days
+      </p>
+    </div>
+  );
+};
 
 export default SuccessfulCheckout;
